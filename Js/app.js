e44bb5f160c5664230f676f64478cbf095f81d9f
Isaac Cavalcante <48898725+isaaccavalcantef@users.noreply.github.com>
2021-10-04 16:18:46 -0300

    Ok

--- Js/app.js
+++ Js/app.js
@@ -1,12 +1,32 @@
 let output = document.getElementById('output')
 let buttons = document.getElementsByClassName('tool--btn')
 
-console.log(buttons);
+
+
+console.log(buttons)
 
 for(let btn of buttons) {
     btn.addEventListener('click', () => {
         let cmd = btn.dataset['x']
-        
+
+      
+
         document.execCommand(cmd, false, null)
+
+        
+        
     })
+}
+
+
+function onlynumber(evt) {
+   var theEvent = evt || window.event;
+   var key = theEvent.keyCode || theEvent.which;
+   key = String.fromCharCode( key );
+   //var regex = /^[0-9.,]+$/;
+   var regex = /^[0-9.]+$/;
+   if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+   }
 }
